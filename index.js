a482f2be4302120dda44a8735bb80e67a00ac9f6
ConnectAcademy <admen.sherifoski@ricci.mk>
2022-03-23 20:07:40 +0100

    [UPDATE] Added a very important comment

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
-let maxEl = document.getElementById("max-el")
-let isOddEl = document.getElementById("is-odd-el")
-let count = 0
-let maxEntry = 0
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let maxEl = document.getElementById("max-el");
+let isOddEl = document.getElementById("is-odd-el");
+let count = 0;
+let maxEntry = 0;
 
-console.log(isOddEl)
+// a very important comment
+
+console.log(isOddEl);
 
 function increment() {
-    count++;
-    countEl.innerHTML = count
+  count++;
+  countEl.innerHTML = count;
 }
 
 function save() {
-    console.log(saveEl.innerText)
-    if (!saveEl.innerText) {
-        prevEntry = countEl.innerText
-    } else {
-        prevEntry = saveEl.innerText + " - " + countEl.innerText
-    }
+  console.log(saveEl.innerText);
+  if (!saveEl.innerText) {
+    prevEntry = countEl.innerText;
+  } else {
+    prevEntry = saveEl.innerText + " - " + countEl.innerText;
+  }
 
-    if (maxEntry < count) {
-        maxEntry = count
-    }
+  if (maxEntry < count) {
+    maxEntry = count;
+  }
 
-    if (count == 0) {
-        isOddEl.innerText = "0 entries, nor even nor odd"
-    } else if (count % 2 == 0) {
-        isOddEl.innerText = "Even"
-    } else {
-        isOddEl.innerText = "Odd"
-    }
+  if (count == 0) {
+    isOddEl.innerText = "0 entries, nor even nor odd";
+  } else if (count % 2 == 0) {
+    isOddEl.innerText = "Even";
+  } else {
+    isOddEl.innerText = "Odd";
+  }
 
-    maxEl.innerText = maxEntry
-    saveEl.innerText = prevEntry
-    countEl.innerText = 0
-    count = 0
+  maxEl.innerText = maxEntry;
+  saveEl.innerText = prevEntry;
+  countEl.innerText = 0;
+  count = 0;
 }
